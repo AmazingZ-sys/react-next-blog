441bec113b4c7c3110b379e6a20cd0c2efdf4f43
AmazingZ-sys <1085266067@qq.com>
2021-04-07 19:20:57 +0800

    前端环境初始化

---
+++ front-end/components/Header.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import styles from '../styles/components/Header.module.scss'
+import {
+  HomeOutlined,
+  YoutubeOutlined,
+  CustomerServiceOutlined,
+  SettingFilled,
+  SmileOutlined,
+  SyncOutlined,
+  LoadingOutlined,
+
+} from '@ant-design/icons';
+
+import {Row,Col, Menu} from 'antd'
+const Header = () => (
+  <div className={styles.header}>
+    <Row type="flex" justify="center">
+      <Col  xs={24} sm={24} md={10} lg={15} xl={12}>
+        <span className={styles.header_logo}>AmazingZ</span>
+        <span className={styles.header_text}>人生碌碌，竟短论长。</span>
+      </Col>
+
+      <Col className={styles.memu_div} xs={0} sm={0} md={14} lg={8} xl={6}>
+        <Menu  mode="horizontal">
+          <Menu.Item key="home">
+            <HomeOutlined />
+            首页
+          </Menu.Item>
+          <Menu.Item key="video">
+            <YoutubeOutlined />
+            视频
+          </Menu.Item>
+          <Menu.Item key="life">
+            <CustomerServiceOutlined />
+            生活
+          </Menu.Item>
+        </Menu>
+      </Col>
+    </Row>
+  </div>
+)
+
+export default Header
